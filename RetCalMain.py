9df199020a1e30a4330facdc77ea136f1183d19c
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2021-10-04 18:16:30 -0500

    Remove PyQt5 UI warning

--- RetCalMain.py
+++ RetCalMain.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\pythonstuff\retraction.ui'
-#
-# Created by: PyQt5 UI code generator 5.13.2
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 from RetCalui import Ui_MainWindow
 from decimal import Decimal
